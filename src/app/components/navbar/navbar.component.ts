b1aee36f04d9c680c2d730ca9858fa29541a343d
Alberto Arias <alberto@boodland.com>
2017-11-20 17:18:18 +0100

    Move navbar component within app component except the navbar header Open sidenav from navbar header

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +9,13 @@ export class NavbarComponent implements OnInit {
 
   title: String = 'Subject Hub';
 
+  @Output() menuClickEvent: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
+
+  menuClick() {
+    this.menuClickEvent.emit();
+  }
 
   ngOnInit() {
   }
