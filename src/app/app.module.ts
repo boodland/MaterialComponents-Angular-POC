7da126260cd134ab7e8fbec294f46f9238a91419
Alberto Arias <alberto@boodland.com>
2017-11-22 18:09:51 +0100

    Reorder imported modules within app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './shared/material.module';
 import { AppRoutingModule } from './shared/app-routing.module';
+import { MaterialModule } from './shared/material.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { SubjectModule } from './subject/subject.module';
@@ -17,8 +17,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
     AppRoutingModule,
+    MaterialModule,
     NavigationModule,
     DashboardModule,
     SubjectModule
