5af3c434ff0838c6dd71d714217e2e9b105488b1
Alberto Arias <alberto@boodland.com>
2017-11-22 15:06:45 +0100

    Move navbar-actions component within navigation module and update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-import { NavbarActionsComponent } from './components/navbar-actions/navbar-actions.component';
 import { SubjectsSummaryComponent } from './components/subjects-summary/subjects-summary.component';
 import { SubjectSummaryService } from './services/subject-summary.service';
 import { AppRoutingModule } from './shared/app-routing.module';
@@ -17,7 +16,6 @@ import { NavigationModule } from './navigation/navigation/navigation.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarActionsComponent,
     SubjectsSummaryComponent,
     SubjectDetailsComponent
   ],
--- src/app/navigation/navigation/navigation.module.ts
+++ src/app/navigation/navigation/navigation.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { MaterialModule } from '../../shared/material.module';
+import { AppRoutingModule } from '../../shared/app-routing.module';
+
 import { NavbarHeaderComponent } from './components/navbar-header/navbar-header.component';
+import { NavbarActionsComponent } from './components/navbar-actions/navbar-actions.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
-  declarations: [NavbarHeaderComponent],
-  exports: [NavbarHeaderComponent]
+  imports: [CommonModule, MaterialModule, AppRoutingModule],
+  declarations: [NavbarHeaderComponent, NavbarActionsComponent],
+  exports: [NavbarHeaderComponent, NavbarActionsComponent]
 })
 export class NavigationModule { }
