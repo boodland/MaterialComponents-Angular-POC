e29621db243df50d8f35157b3e30f24801ed04f9
Alberto Arias <alberto@boodland.com>
2017-11-28 12:19:41 +0100

    Update subject routing with the child configuration Update routerLink path to match the new configuration

--- src/app/subject/subject-routing.module.ts
+++ src/app/subject/subject-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SubjectComponent } from './subject.component';
 import { SubjectListComponent } from './components/subject-list/subject-list.component';
 import { SubjectDetailsComponent } from '../subject/components/subject-details/subject-details.component';
 
 const subjectRoutes: Routes = [
-  { path: 'subjects', component: SubjectListComponent, data: { state: 'subjects' } },
-  { path: 'subject/:id', component: SubjectDetailsComponent, data: { state : 'subject-details'}  }
+  {
+    path: 'subjects', component: SubjectComponent,
+    children: [
+      { path: '', component: SubjectListComponent, data: { state: 'subjects-list' } },
+      { path: ':id', component: SubjectDetailsComponent, data: { state : 'subject-details'}  }
+    ]
+  }
 ];
 
 @NgModule({
