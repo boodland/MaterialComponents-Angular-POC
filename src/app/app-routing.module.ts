f35a8650fde19b10b33dbc5a8e4223ab7f605e5a
Alberto Arias <alberto@boodland.com>
2017-12-05 22:20:18 +0100

    Create secoundary dialog route within named outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DialogMessageComponent } from './dialog/components/dialog-message/dialog-message.component';
+
 const appRoutes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'subjects', loadChildren: 'app/subject/subject.module#SubjectModule' },
   { path: 'tutors', loadChildren: 'app/tutor/tutor.module#TutorModule' },
+  { path: 'dialog', component: DialogMessageComponent, outlet: 'dialog' },
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from './app-material.module';
 import { LoaderModule } from './loader/loader.module';
+import { DialogModule } from './dialog/dialog.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +22,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     AppMaterialModule,
     LoaderModule,
+    DialogModule,
     NavigationModule,
     DashboardModule,
     AppRoutingModule
